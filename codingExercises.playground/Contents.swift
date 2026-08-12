0404b3be51aa4961f5c3c829dbf67211177198d2
Gabriel Ramos <gabrielfco02@hotmail.com>
2026-08-11 22:21:36 -0700

    feat: add function exercises to playground

--- codingExercises.playground/Contents.swift
+++ codingExercises.playground/Contents.swift
@@ -8,9 +8,10 @@ import UIKit
  */
 
 let asistencias: Int = 10
-let totalClases:Int = 20
+let totalClases: Int = 20
 
-var porcentajeAsistencia:Double = (Double(asistencias)/Double(totalClases)) * 100
+var porcentajeAsistencia: Double =
+    (Double(asistencias) / Double(totalClases)) * 100
 
 print("Porcentaje asistencia clases: \(porcentajeAsistencia)%")
 
@@ -27,19 +28,55 @@ var height: Float = 1.68
 var imc = peso / (height * height)
 print("Indice masa corporal \(imc)")
 
-
 /**Cálculo de Descuento:
 Declara dos variables: precioOriginal y porcentajeDescuento.
 Calcula el precio después del descuento utilizando la fórmula: PrecioDescuento = PrecioOriginal - (PrecioOriginal *
 PorcentajeDescuento / 100).
 Imprime el precio original y el precio con descuento.*/
 
-
 let originalPrice = 17.99
-let discountPercent:Double = 20
+let discountPercent: Double = 20
 var discountPrice = originalPrice - (originalPrice * discountPercent / 100)
 
-print("dprecio original: \(originalPrice), precio con descuento: \(discountPrice)")
-
+print(
+    "dprecio original: \(originalPrice), precio con descuento: \(discountPrice)"
+)
 
 // MARK: Funciones
+func showMyName() {
+    print("Hola, primera funcion")
+}
+
+showMyName()
+
+// MARK: Funciones con parametro de entrada y salida
+
+func showMyCustomName(customName: String) -> String {
+    return "Hola \(customName)"
+}
+
+showMyCustomName(customName: "Gabriel")
+
+func calculate(a: Int, b: Int) {
+    let result = a + b
+    // print("El resultado es: \(result)")
+    print("Resultado es: \(a + b)")
+}
+
+calculate(a: 5, b: 10)
+
+// _ -> "me da igual que tenga que asignarle el nombre". es pasar el parametro sin poner a: y b:
+func calculate2(_ a: Int, _ b: Int) {
+    let result = a + b
+    // print("El resultado es: \(result)")
+    print("Resultado es: \(a + b)")
+}
+
+
+calculate2(10, 20)
+// -> tipo de valor que tiene que retornar
+func calculate3(a: Int, b:Int) -> Int {
+    let result = a + b
+    print("resultado: \(result)")
+    return result
+}
